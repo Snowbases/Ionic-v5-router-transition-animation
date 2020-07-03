39e1eb136dd2bdb27ffdfaeb001c1b919471b4cd
SnowBases <syid98@gmail.com>
2020-07-03 17:51:34 +0800

    Example router animation

---
+++ src/app/details/details.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.page.html',
+  styleUrls: ['./details.page.scss'],
+})
+export class DetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,70 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  // This code was inspired from mhartington's github repo, https://github.com/mhartington/v5-animations
+  slideRouterAnimation = (_: HTMLElement, opts: any) => {
+    const backDirection = opts.direction === 'back';
+    const enteringEl = opts.enteringEl;
+    const leavingEl = opts.leavingEl;
+
+    const getIonPageElement = (element: HTMLElement) => {
+      if (element.classList.contains('ion-page')) {
+        return element;
+      }
+
+      const ionPage = element.querySelector(
+        ':scope > .ion-page, :scope > ion-nav, :scope > ion-tabs'
+      );
+      if (ionPage) {
+        return ionPage;
+      }
+      return element;
+    };
+
+    const enteringPageEl = getIonPageElement(enteringEl);
+
+    const rootTransition = this.animationController.create();
+
+    const enterTransition = this.animationController.create();
+    const leavingTransition = this.animationController.create();
+
+    leavingTransition
+      .addElement(getIonPageElement(leavingEl))
+      .duration(540);
+
+    enterTransition
+      .addElement(enteringPageEl)
+      .duration(540)
+      .fill('both')
+      .beforeRemoveClass('ion-page-invisible');
+
+    if (!backDirection) {
+      enterTransition
+        .beforeStyles({ shadow: '5px 6px 20px rgba(0, 0, 0, 0.15)' })
+        .easing('cubic-bezier(0.32,0.72,0,1)')
+        .keyframes([
+          { offset: 0, transform: 'translateY(100vh)' },
+          { offset: 1, transform: 'translateY(0)' }
+        ])
+        .afterClearStyles(['shadow']);
+    } else {
+      leavingTransition
+        .beforeStyles({ shadow: '5px 6px 20px rgba(0, 0, 0, 0.15)' })
+        .easing('cubic-bezier(0.32,0.72,0,1)')
+        .keyframes([
+          { offset: 0, transform: 'translateY(0)' },
+          { offset: 1, transform: 'translateY(100vh)' }
+        ])
+        .afterClearStyles(['shadow']);
+    }
+
+    rootTransition.addAnimation([enterTransition, leavingTransition]);
+
+    return rootTransition;
+  };
+
+  constructor(
+    public animationController: AnimationController
+  ) { }
 
 }
